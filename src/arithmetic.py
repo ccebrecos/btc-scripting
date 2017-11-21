97160723b6d827a0b80b377572d7f55d600ff69b
Carlos GC <mail@ccebrecos.com>
2017-11-21 13:03:32 +0100

    Added arithmetic test skeleton

---
+++ src/arithmetic.py
@@ -0,0 +1,60 @@
+import sys
+# OPCODES to use
+from btc_framework.bitcoin import OP_2MUL, OP_2DIV, OP_MUL, OP_DIV, OP_MOD, \
+                                  OP_NUMEQUAL, OP_NEVF, OP_NUMEQUALVERIFY
+# BTC related
+from btc_framework.bitcoin import SignableTx, TxInput, TxOutput, script, \
+                                    address, VarInt
+
+
+if __name__ == "__main__":
+    # read params
+    keys_base58 = sys.argv[1:]
+    keys = [address.WIF.decode(key) for key in keys_base58]
+    sign_key = keys[0]
+
+    # transaction related params
+    utxo_id = bytes().fromhex(
+        "")
+    utxo_vout, utxo_value = 0, 0
+    fees = 0.005
+    to_pay = utxo_value - fees
+    to_pay_addr = address.P2PKH(public_key=sign_key.public_key)
+
+    # create new transaction
+    transaction = SignableTx()
+
+    # fill transaction
+    # add inputs
+    in0 = TxInput(utxo_id, utxo_vout, script.sig.P2PKH())
+    in0.script.input = in0
+    transaction.add_input(in0)
+
+    # add outputs
+    test_script = script.Script([])
+    transaction.add_output(TxOutput(test_script, btc=to_pay))
+
+    # sign
+    transaction.inputs[0].script.sign(key=sign_key.private_key)
+
+    # return transaction created
+    print(transaction)
+    print(transaction.serialize().hex())
+
+    # SPEND THE PREVIOUS TRANSACTION
+    # transaction related params
+    utxo_id, utxo_vout, to_pay = transaction.id, 0, to_pay - fees
+
+    # create new transaction
+    spendtx = SignableTx()
+
+    # fill transaction
+    # add inputs
+    spend_script = script.Script([])
+    spendtx.add_input(TxInput(utxo_id, utxo_vout, spend_script))
+    # add outputs
+    spendtx.add_output(TxOutput(to_pay_addr.script, btc=to_pay))
+
+    # return transaction created
+    print(spendtx)
+    print(spendtx.serialize().hex())
